04d47d6da1fe87be09c0a5ccf6ca74a8533f8601
Emre Karayaman <karayaman.emre@outlook.com>
2024-03-23 13:58:12 +0300

    Update font weight in Navbar component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function Navbar() {
 	return (
 		<nav className='fixed top-0 left-0 w-full h-[72px] flex items-center justify-between px-4 bg-white border-b border-[#EAECF0]'>
-			<h2 className='text-[#145389] leading-6 text-[20px] font-bold'>
+			<h2 className='text-[#145389] leading-6 text-[20px] font-semibold'>
 				kargakarga
 			</h2>
 			<div className='flex items-center justify-center gap-4 text-[#667085]'>
